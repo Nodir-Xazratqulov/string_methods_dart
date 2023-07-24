a9989a1fb358a76367a589bd6835497b6c3d3a6c
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:20:01 +0500

    str-method-07

--- bin/string_methods07.dart
+++ bin/string_methods07.dart
@@ -1,3 +1,14 @@
 //Task 7:
 //Create a function that takes a word as input and checks if it contains the letter "x" (case-sensitive). If it does, return "Found 'x'!"; otherwise, pad the word on the right with "x" until it contains "x," and return the modified word.
-void main() {}
+
+
+func(String s){
+  if (s.contains('x')) {
+    return 'X topildi';
+  } else{
+    return s.padRight(s.length+1, 'x');
+  }
+}
+void main() {
+  print(func('hammaga salom'));
+}
