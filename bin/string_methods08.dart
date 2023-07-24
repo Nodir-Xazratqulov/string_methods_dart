f23f58693851a37d0272bdbdf66c163dd4ff7d59
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:20:19 +0500

    str-method-08

--- bin/string_methods08.dart
+++ bin/string_methods08.dart
@@ -1,3 +1,13 @@
 //Task 8:
 //Write a function that takes a sentence as input and checks if it contains the word "awesome" (case-sensitive). If it does, return the index of the last occurrence of "awesome" in the sentence; otherwise, return -1.
-void main() {}
+
+
+func(String s){
+  if (s.contains('ajoyib')) {
+    return s[s.length-1];
+  } 
+  return -1;
+}
+void main() {
+  print(func('ajoyib salom'));
+}
