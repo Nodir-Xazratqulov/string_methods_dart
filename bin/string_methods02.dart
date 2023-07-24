2f58e09b69be36aa5716e859e69b0bc8e2f56682
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:18:23 +0500

    str-method-02

--- bin/string_methods02.dart
+++ bin/string_methods02.dart
@@ -1,3 +1,13 @@
 //Task 2:
 //Write a function that takes a message as input and checks if it contains the word "happy" (case-sensitive). If it does, return "I'm glad you're happy!"; otherwise, return "What's making you sad?"
-void main() {}
+
+func(String s){
+  if (s.contains('baxtli')) {
+    return 'Baxtli ekanligingizdan xursandman';
+  } else {
+    return 'Sizni nima qiynayapti';
+  }
+}
+void main() {
+  print(func('men baxtliman'));
+}
