a02aa199fe425013e2e82c124dccf5231df295b0
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:18:53 +0500

    str-method-03

--- bin/string_methods03.dart
+++ bin/string_methods03.dart
@@ -1,3 +1,14 @@
 //Task 3:
 //Implement a function that takes a password as input. If the password's length is less than 8 characters, pad it on the right with "!" until it becomes 8 characters. Return the modified password.
-void main() {}
+
+
+func(String s){
+  if (s.length<=8) {
+    return s.padLeft(8, '!');
+  } else{
+    return s;
+  }
+}
+void main() {
+  print(func('as'));
+}
