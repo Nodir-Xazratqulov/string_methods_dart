537d70dd8aaf0eac216d8f7259e46acf5a5de88b
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:19:21 +0500

    str-method-05

--- bin/string_methods05.dart
+++ bin/string_methods05.dart
@@ -1,3 +1,13 @@
 //Task 5:
 //Write a function that takes a word as input and returns it in lowercase. If the word contains the letter "a," pad it on the left with "a" until "a" is the first letter.
-void main() {}
+
+func(String s){
+  if (s.contains('a')) {
+    return s.padLeft(s.length+1, 'a');
+  } else{
+    return -1;
+  }
+}
+void main() {
+  print(func('aqwera'));
+}
