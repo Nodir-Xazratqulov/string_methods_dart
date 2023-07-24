05a0f3db33a8ff0dff9b3a511b413ce35ab57d5b
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:19:36 +0500

    str-method-06

--- bin/string_methods06.dart
+++ bin/string_methods06.dart
@@ -1,3 +1,15 @@
 //Task 6:
 //Implement a function that takes a sentence as input and checks if it contains both "happy" and "birthday" (case-sensitive). If it does, return "Happy Birthday!"; otherwise, return "Have a great day!"
-void main() {}
+
+func(String text){
+  String a = 'tugilgan kuningiz bilan!';
+  String b = 'hayrli kun';
+  if (text.contains('baxtli') && text.contains('tugilgan kun')) {
+    return a.toString();
+  } else{
+    return b;
+  }
+}
+void main() {
+  print(func('men baxtliman chunki bugun tugilgan kunim'));
+}
