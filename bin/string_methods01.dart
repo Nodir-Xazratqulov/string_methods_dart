242cc7abfd37eee2a3be539251d9e485310ef835
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:18:10 +0500

    str-method-01

--- bin/string_methods01.dart
+++ bin/string_methods01.dart
@@ -1,3 +1,10 @@
 //Task 1:
 //Create a function that takes a name as input and returns it in uppercase.
-void main() {}
+
+String func(String s){
+  return s.toUpperCase();
+}
+
+void main() {
+  print(func('hello'));
+}
