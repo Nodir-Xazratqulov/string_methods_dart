d866c486926832cff34d96dbdfae448e4a70acad
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:20:38 +0500

    str-method-09

--- bin/string_methods09.dart
+++ bin/string_methods09.dart
@@ -1,3 +1,14 @@
 //Task 9:
 //Implement a function that takes a message as input and returns it in uppercase, but if it contains "cool," change only the first occurrence of "cool" to "AMAZING" before converting to uppercase.
-void main() {}
+
+
+String func(String message) {
+  if (message.contains('salqin')) {
+    message = message.replaceFirst('salqin', 'AJOYIB');
+  }
+  
+  return message.toUpperCase();
+}
+void main() {
+  print(func('bugun havo salqin'));
+}
