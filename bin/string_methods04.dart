57c1792e383dba849db5b90d25b849c8e0d5d48e
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:19:08 +0500

    str-method-04

--- bin/string_methods04.dart
+++ bin/string_methods04.dart
@@ -1,3 +1,13 @@
 //Task 4:
 //Create a function that takes a sentence as input and checks if it contains the word "hello" (case-sensitive). If it does, return the index of the first occurrence of "hello" in the sentence; otherwise, return -1.
-void main() {}
+
+func(String s){
+  if (s.contains('salom')) {
+    return s[0];
+  } else {
+    return -1;
+  }
+}
+void main() {
+  print(func('salomu alaykum'));
+}
