0fbc8cde827ba68705e451e2bc68b7d028fbf861
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-24 12:20:56 +0500

    str-method-10

--- bin/string_methods10.dart
+++ bin/string_methods10.dart
@@ -1,3 +1,18 @@
 //Task 10:
 //Create a function that takes a sentence as input and checks if it contains both "happy" and "day" (case-sensitive). If it does, return "Have a Happy Day!"; otherwise, return "Keep smiling!"
-void main() {}
+
+
+
+
+String func(String message) {
+  if (message.contains('baxtli') && message.contains('kun')) {
+    message = 'Baxtli kun';
+  } else{
+    message = 'Tabassumni davom ettiring';
+  }
+  
+  return message.toUpperCase();
+}
+void main() {
+  print(func('baxtli kunlarimdan biri bugun'));
+}
